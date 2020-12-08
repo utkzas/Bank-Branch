e178a420d2249309a45d895ae40917c3833c49ff
Utkarsh Sharma <46284927+utkzas@users.noreply.github.com>
2020-12-08 13:13:54 +0530

    README.md

--- raw/poc/request.js
+++ raw/poc/request.js
@@ -8,14 +8,14 @@ req("https://www.goldmansachs.com/our-firm/locations.html",answerOFurl);
 function answerOFurl(err,res,html)
 {   
     let sTool=ch.load(html);
-    let BranchArr=sTool("ul.listings.filter"); 
+    let BranchArr=sTool("ul.listings.filter"); //extract branches from list
     let fullhtml="<ul>";
     
-    for(let i=0;i<1;i++) h
-    {    let allData=sTool(BranchArr); 
+    for(let i=0;i<11;i++) //there are 11 asia_pacific branch
+    {    let allData=sTool(BranchArr[i]).find(li.class.filter_asiapacific) //list items with li class= asiapacific
          fullhtml+=sTool(allData).html();
     }
-        fullhtml+="</ul>" //unordered list
+        fullhtml+="</ul>"
     
     fs.writeFileSync("GOLDMAN-SACHS-branches.html",fullhtml);
     console.log("Files Saved Successfully!");
